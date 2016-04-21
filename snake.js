0bdf7ac1a5e8dbc81deb1a13aecad13410632ea7
Ghusar <201401167@daiict.ac.in>
2016-04-21 14:49:47 -0700

    new looks and fixed bug of start button and stuck

--- snake.js
+++ snake.js
@@ -1,10 +1,13 @@
-function snake(x,y){
+function snake(x,y,color){
 	this.x = x;
 	this.y = y;
+	this.color = color;
 }
+
+
 function setup(){
 	
-	directionind = 3;
+	directionind = 0;
 	//alert("hello");
 	sp = 100;
 	sc = 0;
@@ -12,15 +15,23 @@ function setup(){
 	s.innerHTML = sc;
 	can = document.getElementById("can");
 	cont = can.getContext("2d");
+	alertbox = document.getElementById("alertbox");
+	alertm = document.getElementById("alertmessage");
+	
+	can.style.display="block";
+	alertbox.style.display="none";
+	
+	cont.clearRect(0,0,300,500);
 	list = new Array();
-	var head = new snake(30,0);
+	list = [];
+	var head = new snake(30,0,"black");
 	list.push(head);
-	var head = new snake(20,0);
+	var head = new snake(20,0,"white");
 	list.push(head);
 	//alert(list[0].x);
-	head = new snake(10,0);
+	head = new snake(10,0,"green");
 	list.push(head);
-	head = new snake(0,0);
+	head = new snake(0,0,"blue");
 	list.push(head);
 	
 	//alert("mo");
@@ -54,12 +65,14 @@ function pause(){
 
 function setvisible(){
 	for(var i = 0; i < list.length ; i++){
+		cont.fillStyle = list[i].color;
 		cont.fillRect(list[i].x,list[i].y,10,10);
 	}
 }
 
 
 function move_right(){
+	if(directionind!=4){
 	directionind = 3;
 	cont.clearRect(list[0].x,list[0].y,10,10);
 	
@@ -67,14 +80,14 @@ function move_right(){
 	var py = list[0].y;
 	
 	list[0].x=list[0].x+10;
-	
+	cont.fillStyle = list[0].color;
 	cont.fillRect(list[0].x,list[0].y,10,10);
 	
 	moveall(px,py);
 	
 	
 	check_it();
-	
+	}
 }
 
 function moveall(px,py){
@@ -82,6 +95,7 @@ function moveall(px,py){
 	for(var l = 1; l < list.length ;l++){
 			if(list[l].x!=goal_x||list[l].y != goal_y)
 			cont.clearRect(list[l].x,list[l].y,10,10);
+			cont.fillStyle = list[l].color;
 			cont.fillRect(px,py,10,10);
 			
 			var temp = list[l].x;
@@ -100,15 +114,18 @@ function check_it(){
 	checkcat();
 	if(list[0].x >300||list[0].y > 500||list[0].x<0||list[0].y<0){
 		clearInterval(moving);
-		alert("finish");
+		alertbox.style.display="block";
+			can.style.display="none";
+			alertm.innerHTML="your snake got stuck";
 	}
 	else{
 	if((list[0].x==goal_x&&list[0].y) ==goal_y&&directionind==3){
 		//alert("aya");
-		insertfirst(new snake(list[0].x+10,list[0].y));
+		insertfirst(new snake(list[0].x+10,list[0].y,"white"));
 			
 			cont.fillRect(list[0].x,list[0].y,10,10);
 			sc = sc + 10;
+			sp = sp - 1;
 			s.innerHTML = sc;
 			goal();
 
@@ -119,6 +136,7 @@ function check_it(){
 			
 			cont.fillRect(list[0].x,list[0].y,10,10);
 			sc = sc + 10;
+			sp = sp - 1;
 			s.innerHTML = sc;
 			goal();
 
@@ -129,6 +147,7 @@ function check_it(){
 			
 			cont.fillRect(list[0].x,list[0].y,10,10);
 			sc = sc + 10;
+			sp = sp - 1;
 			s.innerHTML = sc;
 			goal();
 
@@ -139,6 +158,7 @@ function check_it(){
 			
 			cont.fillRect(list[0].x,list[0].y,10,10);
 			sc = sc + 10;
+			sp = sp - 1;
 			s.innerHTML = sc;
 			goal();
 
@@ -152,7 +172,10 @@ function checkcat(){
 	for(var n = 1; n<list.length;n++){
 		if(list[0].x == list[n].x&&list[0].y == list[n].y){
 			clearInterval(moving);
-			alert("sorry snake stuck");
+			alertbox.style.display="block";
+			can.style.display="none";
+			alertm.innerHTML="your snake got stuck";
+			
 		}
 	}
 }
@@ -185,6 +208,7 @@ function setting(){
 }
 
 function movedown(){
+	if(directionind!=1){
 	directionind= 2;
 	cont.clearRect(list[0].x,list[0].y,10,10);
 	var px = list[0].x;
@@ -193,9 +217,11 @@ function movedown(){
 	cont.fillRect(list[0].x,list[0].y,10,10);
 	moveall(px,py);
 	check_it();
+	}
 }
 
 function moveup(){
+	if(directionind!=2){
 	directionind=1;
 	cont.clearRect(list[0].x,list[0].y,10,10);
 	var px = list[0].x;
@@ -205,9 +231,11 @@ function moveup(){
 	cont.fillRect(list[0].x,list[0].y,10,10);
 	moveall(px,py);
 	check_it();
+	}
 }
 
 function moveleft(){
+	if(directionind!=3){
 	directionind = 4;
 	cont.clearRect(list[0].x,list[0].y,10,10);
 	var px = list[0].x;
@@ -217,6 +245,7 @@ function moveleft(){
 	cont.fillRect(list[0].x,list[0].y,10,10);
 	moveall(px,py);
 	check_it();
+	}
 }
 
 
@@ -225,25 +254,33 @@ window.onkeydown = function() {
 switch (window.event.keyCode) {
 case 37:
 {
+	if(directionind!=3){
 	clearInterval(moving);
 moving = setInterval(moveleft,sp);
+	}
 }
 break;
 case 38:
 {
+	if(directionind!=2){
 	clearInterval(moving);
 moving = setInterval(moveup,sp);
+	}
 }
 break;
 case 39:
 {
+	if(directionind!=4){
 	clearInterval(moving);
 moving = setInterval(move_right,sp);
 } 
+}
 break;
 case 40:{
+	if(directionind!=1){
 	clearInterval(moving);
 moving = setInterval(movedown,sp);
+}
 }
 break;
 }
